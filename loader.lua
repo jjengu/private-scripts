e4cd1fb5bbb5f0f0efb17867fa7d24d7dc57e48c
jengu <124980843+jjengu@users.noreply.github.com>
2025-08-28 17:52:08 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,7 @@ local Scripts = {
     [11515893037] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/stfobtb/script.lua",
     [13594360346] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/stfobtb/script.lua",
     [109983668079237] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/steal-a-brainrot/main.lua",
+    [96342491571673] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/steal-a-brainrot/main.lua",
 }
 local URL = Scripts[game.PlaceId]
 
