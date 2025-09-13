8e295772efc20a345e8b95c68065767f4aa4311b
jengu <124980843+jjengu@users.noreply.github.com>
2025-09-13 15:33:42 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,4 @@
 local Scripts = {
-    [6361937392] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/stfobtb/script.lua",
-    [6356763358] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/stfobtb/script.lua",
-    [11515893037] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/stfobtb/script.lua",
-    [13594360346] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/stfobtb/script.lua",
     [109983668079237] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/steal-a-brainrot/main.lua",
     [96342491571673] = "https://raw.githubusercontent.com/jjengu/private-scripts/refs/heads/main/steal-a-brainrot/main.lua",
 }
